820eb358323111955f01911820cbd304b958f1f9
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-04-15 21:26:06 +0530

    Time: 92 ms (6.54%), Space: 45.7 MB (99.97%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int arr [] = new int [n];
+        int k = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = -1;
+            for(int j=1; j<n; j++){
+                int idx = (i+j)%n; 
+                if(nums[idx]>nums[i]){
+                    arr[i] = nums[idx];
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
