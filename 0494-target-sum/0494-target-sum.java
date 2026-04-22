3f6ce1dbc03c29becffd09454fed112ab6b3b330
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-04-22 20:27:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,7 +1,10 @@
 class Solution {
     int n;
+    HashMap<String,Integer> map;
     public int findTargetSumWays(int[] nums, int target) {
         n = nums.length;
+        map = new HashMap<>();
+        //dp = new int[n][target]; 
         return solve(nums,target,0);
     }
     int solve(int[] nums, int tar,int i){
@@ -9,9 +12,12 @@ class Solution {
             if(tar == 0) return 1;
             return 0;
         }
+        String key = i+""+tar;
+        if(map.containsKey(key)) return map.get(key);
         int count = 0;
         count += solve(nums,tar+nums[i],i+1);
         count += solve(nums,tar-nums[i],i+1);
+        map.put(key,count);
         return count;
     }
 }
