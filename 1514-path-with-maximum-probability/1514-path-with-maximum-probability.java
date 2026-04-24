1ad3dd364e2275fa12dc6c8256c1e46da3a975fe
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-04-24 13:08:33 +0530

    Time: 30 ms (68.33%), Space: 72.1 MB (38.68%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -0,0 +1,39 @@
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        List<List<double[]>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double w = succProb[i];
+
+            adj.get(u).add(new double[]{v, w});
+            adj.get(v).add(new double[]{u, w});
+        }
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[1], a[1]));
+        pq.add(new double[]{start_node,1.0});
+        double [] dis = new double[n];
+        Arrays.fill(dis,0.0);
+        dis[start_node] = 1.0;
+        while(!pq.isEmpty()){
+            double [] cur = pq.poll();
+            int u = (int)cur[0];
+            double w = cur[1];
+            if (u == end_node) return w;
+            if (w < dis[u]) continue;
+            for(double [] nei : adj.get(u)){
+                int v = (int)nei[0];
+                double nd = w*nei[1];
+                if(nd < dis[v]) continue;
+                if(nd > dis[v]){
+                    dis[v] = nd;
+                    pq.add(new double[]{v,nd});
+                }
+            }
+        }
+        return 0.0;
+        //return dis[end_node];
+    }
+}
