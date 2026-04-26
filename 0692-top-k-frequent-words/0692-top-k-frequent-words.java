57112e4549ddc27a448dbd1abfc9108d943dc639
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-04-26 16:23:32 +0530

    Time: 9 ms (23.13%), Space: 46.9 MB (9.24%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         List<String> l = new ArrayList<>();
-        TreeMap<String,Integer> map = new TreeMap<>();
-        for(int i = 0; i < words.length; i++){
-            map.put(words[i],map.getOrDefault(words[i],0)+1);
+        TreeMap<String, Integer> map = new TreeMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], map.getOrDefault(words[i], 0) + 1);
         }
-        PriorityQueue<Map.Entry<String, Integer>> pq =
-            new PriorityQueue<>((a, b) -> {
-                int cmp = Integer.compare(b.getValue(), a.getValue());
-                if (cmp != 0) return cmp;
-                return a.getKey().compareTo(b.getKey());
-            });
-
-        for(Map.Entry<String,Integer> entry : map.entrySet()){
-            pq.offer(entry);
-        }
-        for(int i = 0; i < k; i++){
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) -> {
+            int cmp = Integer.compare(b.getValue(), a.getValue());
+            if (cmp != 0)
+                return cmp;
+            return a.getKey().compareTo(b.getKey());
+        });
+        pq.addAll(map.entrySet());
+        for (int i = 0; i < k; i++) {
             l.add(pq.poll().getKey());
         }
         return l;
