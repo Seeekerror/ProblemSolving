dda070c545b1cdc12f2d85f602e8109035fa4dad
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-04-29 01:57:55 +0530

    Time: 44 ms (60.61%), Space: 110.5 MB (68.18%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,22 +1,23 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int m = grid.length, n = grid[0].length;
-        int[] arr = new int[m * n];
-        int idx=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                arr[idx++] = grid[i][j];
+        int n=grid.length;
+        int m=grid[0].length;
+        int arr[]=new int[n*m];
+        int ind=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[ind++]=grid[i][j];
             }
         }
+
         Arrays.sort(arr);
-        //int base = l.get(0);
-        int num = arr[m*n/2];
-        int count=0;
-        for(int i = 0; i<arr.length; i++){
-            int val = arr[i];
-            if((val - num) % x != 0) return -1;
-            count += Math.abs(val-num)/x;
+        int res=arr[arr.length/2];
+        int cnt=0;
+        for(int i:arr){
+            int temp=Math.abs(i-res);
+            if(temp%x!=0) return -1;
+            cnt+=temp/x;
         }
-        return count;
+        return cnt;
     }
 }
