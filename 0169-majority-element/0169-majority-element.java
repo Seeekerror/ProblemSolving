395a5669867d2540c92e3e4d827a0ba66fa89816
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-06-17 22:16:42 +0530

    Time: 1 ms (99.88%), Space: 55.6 MB (59.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // int c = 0;
-        // int ct = 0;
-        int n = nums.length;
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++){
-            int x = nums[i];
-            if(set.contains(x)) continue;
-            int c = 0;
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] == nums[j]) c++;
-                if(c > (n/2)) return nums[j];
+        int count = 0;
+        int can = 0;
+        for(int x : nums){
+            if(count == 0) can = x;
+            if(x == can){
+                count++;
+            }else{
+                count--;
             }
-            set.add(x);
         }
-        return -1;
+        return can;
     }
 }
