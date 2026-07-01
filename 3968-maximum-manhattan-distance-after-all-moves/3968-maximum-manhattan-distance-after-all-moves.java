8adf49936cfa72c1a1df29fcc3ab32a2d7c75865
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-07-01 16:33:21 +0530

    Time: 23 ms (14.24%), Space: 47.9 MB (59.49%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -1,29 +1,27 @@
 class Solution {
     public int maxDistance(String moves) {
-        int u = 0;
-        int l=0;
-        int r=0;
-        int d=0;
+        int y=0;
+        int x=0;
         int space=0;
         for(int i = 0;i < moves.length();i++){
             if(moves.charAt(i) == 'U'){
-                u++;
+                x++;
             }
             else if(moves.charAt(i) == 'L'){
-                l++;
+                y--;
             }
             else if(moves.charAt(i) == 'D'){
-                d++;
+                x--;
             }
             else if(moves.charAt(i) == 'R'){
-                r++;
+                y++;
             }
             else{
                 space++;
             }
         }
-        int ans1 = Math.abs(l-r);
-        int ans2 = Math.abs(d-u);
+        int ans1 = Math.abs(x);
+        int ans2 = Math.abs(y);
         return ans1+ans2+space;
     }
 }
