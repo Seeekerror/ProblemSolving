83758b254eb1cbf46ee922f1a625c22b49ba295f
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-08-02 22:50:39 +0530

    Time: 4 ms (41.87%), Space: 46.6 MB (7.92%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,16 +1,29 @@
 class Solution {
-    int [][] dp;
+    List<List<Integer>> dp;
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        dp = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int [] row : dp) Arrays.fill(row,Integer.MIN_VALUE);
-        return solve(triangle , 0 , 0);
+        dp = new ArrayList<>();
+        for (int i = 0; i < triangle.size(); i++) {
+            List<Integer> ls = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                ls.add(Integer.MIN_VALUE);
+            }
+            dp.add(ls);
+        }
+        return solve(triangle, 0, 0);
     }
-    int solve(List<List<Integer>> tri , int i , int j){
-        if(i >= tri.size() || j >= tri.get(i).size()) return Integer.MAX_VALUE;
-        if(i == tri.size()-1) return tri.get(i).get(j);
-        if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
-        int f = solve(tri , i+1 , j);
-        int s = solve(tri , i+1 , j+1);
-        return dp[i][j] = tri.get(i).get(j) + Math.min(f,s);
+
+    int solve(List<List<Integer>> tri, int i, int j) {
+        if (i >= tri.size() || j >= tri.get(i).size())
+            return Integer.MAX_VALUE;
+        if (i == tri.size() - 1)
+            return tri.get(i).get(j);
+        if (dp.get(i).get(j) != Integer.MIN_VALUE)
+            return dp.get(i).get(j);
+        int f = solve(tri, i + 1, j);
+        int s = solve(tri, i + 1, j + 1);
+        int ans = tri.get(i).get(j) + Math.min(f, s);
+        dp.get(i).set(j, ans);
+        return dp.get(i).get(j);
     }
 }
