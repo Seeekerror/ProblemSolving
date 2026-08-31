cec40f07d9338e82db375688ccb0e524099bdc5f
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-08-31 14:10:31 +0530

    Time: 14 ms (6.05%), Space: 107.7 MB (20.33%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -12,30 +12,24 @@ class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         List<Integer> l = new ArrayList<>();
         ListNode node = head;
-        while (node != null) {
-            l.add(node.val);
-            node = node.next;
-        }
-        List<Integer> ls = new ArrayList<>();
-        int min = Integer.MAX_VALUE;
-        int x = 0;
-        for (int i = 1; i < l.size() - 1; i++) {
-            if (l.get(i - 1) < l.get(i) && l.get(i) > l.get(i + 1)) {
-                ls.add(i);
-                if (x > 0) { min = Math.min(min, ls.get(x) - ls.get(x - 1)); } x++;
-            } else if (l.get(i - 1) > l.get(i) && l.get(i) < l.get(i + 1)) {
-                ls.add(i);
-                if (x > 0) {
-                    min = Math.min(min, ls.get(x) - ls.get(x - 1));
-                }
-                x++;
-
+        ListNode prev = head;
+        int idx=1;
+        while (node.next != null) {
+            if((node.val > prev.val && node.val > node.next.val) || (node.val < prev.val && node.val < node.next.val)){
+                l.add(idx);
             }
+            prev = node;
+            node = node.next;
+            idx++;
         }
-        if (ls.size() < 2)
+        if (l.size() < 2)
             return new int[] { -1, -1 };
-        Collections.sort(ls);
-        int max = ls.get(ls.size() - 1) - ls.get(0);
+        Collections.sort(l);
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<l.size();i++){
+            min = Math.min(min,l.get(i)-l.get(i-1));
+        }
+        int max = l.get(l.size() - 1) - l.get(0);
         return new int[] { min, max };
     }
 }
