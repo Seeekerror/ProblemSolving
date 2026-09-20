a7d274a536ab54dd2fe3960bb0bc3a8f3415cfc6
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-09-20 19:10:58 +0530

    Time: 41 ms (6.95%), Space: 47.3 MB (17.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,26 @@
 class Solution {
-    int n;
     int [][] dp;
     public int coinChange(int[] coins, int amount) {
-        n = coins.length;
-        dp = new int[n+1][amount+1];
+        dp = new int[coins.length][amount+1];
         for(int [] row : dp) Arrays.fill(row,-1);
-        int ans = solve(coins,amount,0);
-        if(ans != Integer.MAX_VALUE) return ans;
+        int ans = solve(coins, amount, coins.length - 1);
+        if (ans != Integer.MAX_VALUE)
+            return ans;
         return -1;
     }
-    int solve(int [] coins, int am, int i){
-        if(i >= n) return Integer.MAX_VALUE;
+
+    int solve(int[] coins, int am, int i) {
+        if (i < 0)
+            return Integer.MAX_VALUE;
         if(am == 0) return 0;
         if(dp[i][am] != -1) return dp[i][am];
-        int skip = solve(coins,am,i+1);
-        int pick=Integer.MAX_VALUE;
-        if(coins[i] <= am){
-            int ans = solve(coins,am-coins[i],i);
-            if(ans != Integer.MAX_VALUE) pick = 1 + ans;
+        int pick = Integer.MAX_VALUE;
+        if (coins[i] <= am) {
+            int res = solve(coins, am - coins[i], i);
+            if (res != Integer.MAX_VALUE)
+                pick = 1 + res;
         }
-        return dp[i][am] = Math.min(skip,pick);
+        int skip = solve(coins, am, i - 1);
+        return dp[i][am] = Math.min(pick, skip);
     }
 }
