22e2d3d8196f50810e2d6ac981fcadb703fd662e
KUMAR SAKSHAM <pradeepkr.saksham@gmail.com>
2026-09-20 23:35:33 +0530

    Time: 2 ms (87.59%), Space: 44.1 MB (55.58%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,37 +1,39 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-
+        int org = 0;
         Queue<int[]> q = new LinkedList<>();
-        int fresh = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 2) q.add(new int[]{i,j});
-                else if(grid[i][j] == 1) fresh++;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1)
+                    org++;
+                if (grid[i][j] == 2) {
+                    q.add(new int[] { i, j });
+                }
             }
         }
-        int minutes = 0;
-        if(fresh == 0) return 0;
-        while(!q.isEmpty()&& fresh > 0){
+        if (org == 0) return 0;
+        int[][] pos = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        int ans = 0;
+        while (!q.isEmpty()) {
             int size = q.size();
-
-            for(int i = 0; i < size; i++){
-                int [] cur = q.poll();
-                int r = cur[0];
-                int c = cur[1];
-                int [][] arr = {{-1,0},{1,0},{0,-1},{0,1}};
-                for(int [] nie : arr){
-                    int nr = r + nie[0];
-                    int nc = c + nie[1];
-
-                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
-                        fresh--;
-                        q.add(new int [] {nr,nc});
+            for (int j = 0; j < size; j++) {
+                int[] x = q.poll();
+                int u = x[0];
+                int v = x[1];
+                for (int i = 0; i < 4; i++) {
+                    int nu = u + pos[i][0];
+                    int nv = v + pos[i][1];
+                    if (nu >= 0 && nu < grid.length && nv >= 0 && nv < grid[0].length && grid[nu][nv] == 1) {
+                        grid[nu][nv] = 2;
+                        q.add(new int[] { nu, nv });
+                        org--;
                     }
                 }
             }
-            minutes++;
+            ans++;
         }
-        return fresh == 0?minutes:-1;
+        if (org == 0)
+            return ans-1;
+        return -1;
     }
-} 
+}
